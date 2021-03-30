5642ef543ff791bcdfac21387d974b5e43a653a8
crazould <53102589+crazould@users.noreply.github.com>
2021-03-30 16:18:26 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      {{ message }}
+    </h1>
+    <input type="text" v-model="message" />
+    <button @click="send()">
+      Add message
+    </button>
+    <ul>
+      <li v-for="message in messages" :key="message">
+        {{ message }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      number: 0,
+      message: "",
+      messages: [],
+    };
+  },
+  sockets: {
+    connect() {
+      console.log("CONNECTED!")
+    },
+    disconnect() {
+    },
+    message(data) {
+      console.log("hallo hallo")
+      this.messages.push(data);
+    },
+  },
+  methods: {
+    send() {
+      // console.log(this.message);
+      this.$socket.emit("message", this.message);
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import socketio from 'socket.io';
+import VueSocketIO from 'vue-socket.io';
+// export const SocketInstance = socketio('http://localhost:5000');
+
+const SocketIO = require("socket.io-client");
 
 Vue.config.productionTip = false
 
+// Vue.use(VueSocketIO, SocketInstance)
+const options = {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+};
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('http://localhost:5000', options), //options object is Optional
+})
+);
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
+
+
